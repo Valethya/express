12dd8f51835daf61a8596d708ecda6339bbac216
Valentina Rojas <valenthinarojasr@gmail.com>
2023-01-27 01:22:09 -0300

    revisando que se modifican los estilos

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -15,29 +15,31 @@ const createHtml = (data) => {
   return data.length
     ? data.map((product) => {
         products.innerHTML += `
-            <div class="product-card">
-                <img src="${product.thumbnails}" alt="${product.title}" class="product-card_image"/>
-                <span class="text-xs opacity-50">${product.id}</span>
-                <h2 class="product-card_title">${product.title}</h2>
-                <p class="text-sm product-card_description">${product.description}</p>
-                <div class="product-card_data">
-                <span class="text-sm opacity-50">PRICE: $${product.price}</span>
-                    <span class="text-sm opacity-50">STOCK: ${product.stock}</span>
-                    <span class="text-sm opacity-50">STATUS: ${product.status}</span>
-                </div>
+            <div class="container-product-card">
+                <div class="product-card">
+                    <img src="${product.thumbnails}" alt="${product.title}" />
+                    <span>${product.id}</span>
+                    <h2>${product.title}</h2>
+                    <p >${product.description}</p>
+                    <div class="detalle">
+                        <span>PRICE: $${product.price}</span>
+                        <span>STOCK: ${product.stock}</span>
+                        <span>STATUS: ${product.status}</span>
+                    </div>
+                </div> 
             </div>
         `;
       })
     : (products.innerHTML += `
             <div class="product-card">
-                <img src="${data.thumbnails}" alt="${data.title}" class="product-card_image"/>
-                <span class="text-xs opacity-50">${data.id}</span>
-                <h2 class="product-card_title">${data.title}</h2>
-                <p class="text-sm product-card_description">${data.description}</p>
-                <div class="product-card_data">
-                    <span class="text-sm opacity-50">PRICE: $${data.price}</span>
-                    <span class="text-sm opacity-50">STOCK: ${data.stock}</span>
-                    <span class="text-sm opacity-50">STATUS: ${data.status}</span>
+                <img src="${data.thumbnails}" alt="${data.title}" />
+                <span>${data.id}</span>
+                <h2>${data.title}</h2>
+                <p >${data.description}</p>
+                <div class="detalle">
+                    <span>PRICE: $${data.price}</span>
+                    <span>STOCK: ${data.stock}</span>
+                    <span>STATUS: ${data.status}</span>
                 </div>
             </div>
         `);
